6390081cfb82d20b85e626143b7a56f4e7910445
Jamaal Sedayao <you@example.com>
2015-10-09 16:18:23 -0400

    added pods, facebook SDK

---
+++ Unit-2-Journal/Unit-2-Journal/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  Unit-2-Journal
+//
+//  Created by Jamaal Sedayao on 10/9/15.
+//  Copyright © 2015 Jamaal Sedayao. All rights reserved.
+//
+
+#import "ViewController.h"
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    FBSDKLoginButton *loginButton = [[FBSDKLoginButton alloc] init];
+    loginButton.center = self.view.center;
+    [self.view addSubview:loginButton];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
