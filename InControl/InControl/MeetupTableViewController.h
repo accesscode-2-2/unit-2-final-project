131d023f5ebffe73b1d9bcb039904ea290a15f20
Artur Lane <arturlan.us@gmail.com>
2015-10-18 16:38:22 -0400

    commit

---
+++ InControl/InControl/MeetupTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  MeetupTableViewController.h
+//  InControl
+//
+//  Created by Artur Lan on 10/18/15.
+//  Copyright © 2015 Diana Elezaj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MeetupTableViewController : UITableViewController
+
+@end
