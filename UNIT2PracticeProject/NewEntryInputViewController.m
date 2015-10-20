6dd2b9a2208bdb4ef2b43af1770efbd1fafb5d25
Felicia Weathers <fel.weathers@gmail.com>
2015-10-20 10:44:31 -0400

    added placeholder text to journalEntryTextViewController

--- UNIT2PracticeProject/NewEntryInputViewController.m
+++ UNIT2PracticeProject/NewEntryInputViewController.m
@@ -20,10 +20,6 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-  
-    [self.addTitle.layer setCornerRadius:30.0f];
-
-    
     // Do any additional setup after loading the view.
     self.entryObject = [[JournalEntryObject alloc] init];
     
@@ -31,23 +27,45 @@
         NSLog(@"We have Photo Data");
     }
     [self.view endEditing:YES];
-
+    
+    self.journalEntryTextView.text = @"Journal Entry";
+    self.journalEntryTextView.textColor = [UIColor lightGrayColor];
+    self.journalEntryTextView.delegate = self;
+    
 }
+- (BOOL) textViewShouldBeginEditing:(UITextView *)textView
+{
+    self.journalEntryTextView.text = @"";
+    self.journalEntryTextView.textColor = [UIColor blackColor];
+    return YES;
+}
+
+-(void) textViewDidChange:(UITextView *)textView
+{
+    
+    if(self.journalEntryTextView.text.length == 0){
+        self.journalEntryTextView.textColor = [UIColor lightGrayColor];
+        self.journalEntryTextView.text = @"Journal Entry";
+        [self.journalEntryTextView resignFirstResponder];
+    }
+}   
+
+
 
 
 
 /*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
+ #pragma mark - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+ // Get the new view controller using [segue destinationViewController].
+ // Pass the selected object to the new view controller.
+ }
+ */
 
 - (IBAction)saveEntryButtonTapped:(id)sender {
-   
+    
     NSLog(@"%@", self.journalEntryTextView.text);
     NSLog(@"%@", self.titleTextField.text);
     
@@ -55,19 +73,19 @@
     self.entryObject.savedTitle = self.titleTextField.text;
     
     self.entryObject.savedImageEntry = [ImageSingleton myImage].image;
-
+    
     
     [[PhotoAlbum sharedPhotoAlbum].photoEntries addObject:self.entryObject];
     
- //   JournalEntryObject* someObject = [[PhotoAlbum sharedPhotoAlbum].photoEntries objectAtIndex:0];
- //   NSLog(@"SAVED TITLE : %@", someObject.savedImageEntry);
+    //   JournalEntryObject* someObject = [[PhotoAlbum sharedPhotoAlbum].photoEntries objectAtIndex:0];
+    //   NSLog(@"SAVED TITLE : %@", someObject.savedImageEntry);
     
-//    UINavigationController *navController = [self.tabBarController.viewControllers objectAtIndex:1];
-//    
-//    EntryListTableViewController* viewcontroller = [navController.viewControllers firstObject];
-//   
-//    
-//    
+    //    UINavigationController *navController = [self.tabBarController.viewControllers objectAtIndex:1];
+    //
+    //    EntryListTableViewController* viewcontroller = [navController.viewControllers firstObject];
+    //
+    //
+    //
     
     [self dismissViewControllerAnimated:NO completion:nil];
     
