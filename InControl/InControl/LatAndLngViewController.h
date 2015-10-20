c265c3af09b6effaa587ee9c21150d7b92425b5d
Artur Lane <arturlan.us@gmail.com>
2015-10-20 00:21:10 -0400

    meetup with lat and lng

---
+++ InControl/InControl/LatAndLngViewController.h
@@ -0,0 +1,13 @@
+//
+//  LatAndLngViewController.h
+//  InControl
+//
+//  Created by Artur Lan on 10/20/15.
+//  Copyright © 2015 Diana Elezaj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LatAndLngViewController : UIViewController
+
+@end
